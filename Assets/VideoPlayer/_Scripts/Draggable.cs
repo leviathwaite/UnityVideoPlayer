7bd342d7818bfb19144f3ba0caa4b286c2dcd400
leviathwaite <theyearofrupert@hotmail.com>
2020-10-30 22:29:48 -0400

    added UILinerRenderer to connect matching test answers

--- Assets/VideoPlayer/_Scripts/Draggable.cs
+++ Assets/VideoPlayer/_Scripts/Draggable.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
 
 // Draggable UI element
 // Returns to intial position when not placed in a slot
@@ -10,6 +12,8 @@ using UnityEngine.UI;
 // TODO add answer so can be evaluatedd
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public UILineRenderer lineRenderer;
+
     [SerializeField]
     private Canvas canvas;
     [SerializeField]
@@ -21,6 +25,21 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     private CanvasGroup canvasGroup;
     private Vector3 startingPosition;
 
+    // for line renderer
+    private List<Vector2> linePoints;
+
+    // How to use UI LineRenderer extension
+    /*
+    // Use this for initialization
+    public void AddNewPoint()
+    {
+        var point = new Vector2() { x = float.Parse(XValue.text), y = float.Parse(YValue.text) };
+        var pointlist = new List<Vector2>(LineRenderer.Points);
+        pointlist.Add(point);
+        LineRenderer.Points = pointlist.ToArray();
+    }
+    */
+
     void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
@@ -28,6 +47,9 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
         // startingPosition = rectTransform.localPosition / canvas.scaleFactor;
         startingPosition = rectTransform.anchoredPosition;
+
+        lineRenderer.LineThickness = 10;
+        linePoints = new List<Vector2>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -60,11 +82,23 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     {
         inSlot = true;
         rectTransform.anchoredPosition = position;
+
+        // Draw line
+        lineRenderer.gameObject.SetActive(true);
+        linePoints.Add(startingPosition);
+        linePoints.Add(rectTransform.anchoredPosition);
+        lineRenderer.Points = linePoints.ToArray();
+        
     }
 
     public void ResetToStartPosition()
     {
         inSlot = false;
         rectTransform.anchoredPosition = startingPosition;
+
+        // TODO is there a better way to clear points?
+        linePoints.Clear();
+        lineRenderer.Points = linePoints.ToArray();
+        lineRenderer.gameObject.SetActive(false);
     }
 }
